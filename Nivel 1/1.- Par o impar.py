19d6c234b48ce85474229ed819f21ec8270efcde
Nieto2020 <nietoaguilar1@outlook.com>
2026-08-16 22:01:37 -0600

    feat: Problema uno resuelto

---
+++ Nivel 1/1.- Par o impar.py
@@ -0,0 +1,13 @@
+num = 0
+option = "y"
+
+while True:
+    num = int(input("Ingrese el numero del 1 al Infinito: "))
+    if num % 2 == 0:
+        print("Numero Par")
+    else:
+        print("Impar")
+    option = input("Continuar? yes = y | no = n")
+    if option != "y":
+        print("ok, by bye")
+        break
