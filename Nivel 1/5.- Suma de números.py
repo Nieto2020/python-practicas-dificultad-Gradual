c9b200633612ed95e42b4886810223b9b71fd6fe
Nieto2020 <nietoaguilar1@outlook.com>
2026-08-20 16:37:41 -0600

    Problema 5 resuelto

---
+++ Nivel 1/5.- Suma de números.py
@@ -0,0 +1,11 @@
+#Dado `N`, calcula la suma:
+#ejmp
+#1 + 2 + 3 + ... + N
+
+#Restricción:** no uses una fórmula matemática; utiliza un ciclo.
+
+n = 7
+
+for i in range(1, n+1):
+    print(i)
+    i=+1
