7482d0e11fd8299dc3c58e77843e6ff3b88f37b3
Nieto2020 <nietoaguilar1@outlook.com>
2026-08-30 21:55:40 -0600

    fix: Carpetas cambiadas

---
+++ Nivel 4 - Diccionarios y funciones/21.- Inventario.py
@@ -0,0 +1,17 @@
+"""Tienes:
+
+python
+inventario = {
+    "manzanas": 10,
+    "peras": 5,
+    "naranjas": 8
+}
+
+
+Crea funciones para:
+
+- agregar productos
+- eliminar productos
+- modificar cantidades
+- consultar cantidad
+- calcular productos totales"""
