d95085c0b0db94b45bd224bb3c17662fec2f3954
Nieto2020 <nietoaguilar1@outlook.com>
2026-09-14 19:24:08 -0600

    Problema 8 corregido

--- Nivel 1 - Fundamentos/8.- Números primos.py
+++ Nivel 1 - Fundamentos/8.- Números primos.py
@@ -4,9 +4,25 @@
 #7 → True
 #10 → False
 
-n = int(input("Ingresa el Numero: "))
+PRIMOS = [2, 3, 5, 7, 11, 13]
+n = [2, 3, 5, 7, 11, 13, 4, 6, 9]
 
-if n % 2 == 1 and n != 1:
-    print("PRIMO")
-else:
-    print("NOT PRIMO")
+for i in n:
+    es_primo = True
+    raiz = int(i ** 0.5)
+
+    if i in PRIMOS:
+        es_primo = True
+    else:
+
+        for p in PRIMOS:
+            if p <= raiz:
+                if i % p == 0:
+                    es_primo = False
+                    break  
+
+    # 3. Resultado final para cada número
+    if es_primo:
+        print(f"{i}: Primo")
+    else:
+        print(f"{i}: Not primo")
