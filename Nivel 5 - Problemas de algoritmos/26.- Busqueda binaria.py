5205e12dab8d7286343b20a9b54337dabdf921b5
Nieto2020 <nietoaguilar1@outlook.com>
2026-09-20 23:43:35 -0600

    fix: nada

---
+++ Nivel 5 - Problemas de algoritmos/26.- Busqueda binaria.py
@@ -0,0 +1,13 @@
+"""Dada una lista ordenada:
+[1, 3, 5, 7, 9, 11, 13, 15]
+
+encuentra un número utilizando búsqueda binaria.
+
+-Restricción: no puedes utilizar `in` ni `index()`; debes implementar el algoritmo."""
+
+lista = [1, 3, 5, 7, 9, 11, 13, 15]
+x = 3
+
+for i in lista:
+    
+    
